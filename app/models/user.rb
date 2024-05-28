43d908324fbf1b3a4402acf9d86274c9b9dc9923
Danaage08 <danaageyt12@gmail.com>
2024-05-28 08:36:21 +0300

    ewewsadas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,7 @@
 class User < ApplicationRecord
   has_many :orders
   has_one :review
-    validates :delivery_address, presence: true, length: { maximum: 255 }
-  validates :return_address, presence: true, length: { maximum: 255 }
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :total_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :user_id, presence: true
-  validate :end_date_after_start_date
-  validate :bikes_present
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
