d9cee6722114add0f2a86a9aedad574de79f87f1
Danaage08 <danaageyt12@gmail.com>
2024-05-28 10:45:37 +0300

    sadsadsadsadsa

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -18,13 +18,21 @@ class Bike < ApplicationRecord
     carbon: "Карбон"
   }
 
-   def available?(start_date, end_date)
-    overlapping_orders = Order.where("admin_status != 'completed' AND (
-      start_date <= :end_date AND end_date >= :start_date
+  def available?(start_date, end_date)
+      # Находим все заказы, которые имеют незавершенный статус и пересекаются с заданным временным интервалом
+    orders = Order.where("admin_status != 'completed' AND (
+      (start_date <= :end_date AND end_date >= :start_date) OR
+      (start_date <= :end_date AND end_date >= :start_date) OR
+      (start_date >= :start_date AND start_date <= :end_date)
     )", start_date: start_date, end_date: end_date)
-    bike_ids_array = self.bike_ids.split(',').map(&:strip).reject(&:empty?)
 
-    overlapping_orders.none? { |order| order.bike_ids.include?(self.id.to_s) }
+    # Получаем все идентификаторы велосипедов из найденных заказов
+    booked_bike_ids = orders.map { |order| order.bike_ids.is_a?(Array) ? order.bike_ids : order.bike_ids.split(',') }.flatten.uniq
+
+    # Находим все велосипеды, которые не были забронированы в указанный период
+    available_bikes = Bike.where.not(id: booked_bike_ids)
+
+    return available_bikes
   end
 
   def self.ransackable_attributes(auth_object = nil)
